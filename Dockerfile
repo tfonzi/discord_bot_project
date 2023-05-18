f7cd735132805ca1cbd1db473d3f6468af7dbb7a
Tyler Fonzi <tyler.a.fonzi@gmail.com>
2023-05-18 15:10:39 -0400

    Made DockerFile more specific in files copied. Added error handling and better parsing with token and discord client connection

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM node:18.14.2-bullseye-slim
 
-COPY . /bot
-
 WORKDIR /bot
+COPY package.json ./
+COPY package-lock.json ./
+COPY tsconfig.json ./
+COPY src ./
+
 RUN npm install
 RUN npm run build
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { ClientOptions, Client, GatewayIntentBits} from "discord.js";
 import * as dotenv from "dotenv";
 
+interface botToken {
+    discordBotToken: string;
+}
+
 const clientOptions: ClientOptions = {intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent]};
 const client = new Client(clientOptions);
 dotenv.config();
@@ -17,5 +21,13 @@ client.on(`messageCreate`, async (msg) => {
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+try {
+    const token: botToken = JSON.parse(process.env.BOT_TOKEN);
+    console.log(`About to log in with ${token.discordBotToken.substring(0,5)}...`);
+    client.login(token.discordBotToken);
+}
+catch(err) {
+    console.error(err);
+}
+
  
